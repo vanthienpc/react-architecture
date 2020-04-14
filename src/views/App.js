6adadadfd00bbe29b23779acafee3554810f3186
Thien Vo <thien.vo@tyme.com>
2020-04-14 18:53:44 +0700

    Initialize redux js

--- src/views/App.js
+++ src/views/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from '../logo.svg';
-import './App.css';
+import { ConnectedRouter } from 'connected-react-router';
+import { Route, Switch } from 'react-router-dom';
+import { history } from 'stores/rootStore';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Home = React.lazy(() => import('./pages/Home'));
+
+const App = () => (
+  <ConnectedRouter history={history}>
+    <React.Suspense fallback="loading..." isActive={true}>
+      <Switch>
+        <Route exact={true} path="/" component={Home} />
+      </Switch>
+    </React.Suspense>
+  </ConnectedRouter>
+);
 
 export default App;
